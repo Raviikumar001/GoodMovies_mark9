e1a468713c09d720835690fa869f66b0b84d5994
Raviikumar001 <samcollins9899@gmail.com>
2021-08-25 15:25:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import Navbar from "./components/navbar";
+import netli from '../src/images/netfli.jpg'
 
-function App() {
+const Movies = {
+  Marvel: [
+    { name: "Doctor Strange", rating: "4/5" },
+    { name: "Iron Man", rating: "3.5/5" },
+    {
+      name: "Black Panther", rating: "3.8/5"
+    }
+  ],
+
+  HollyWoodMovie: [
+    {
+      name: "Slumdog millionare",
+      rating: "4.8/5"
+    },
+    {
+      name: "Harry Potter and the Deathly Hallow's",
+      rating: "4.5/5"
+    },
+    {
+      name:"The Maze Runner",
+      rating: "3.4/5"
+    }
+  ],
+  Fantasy: [
+    {
+      name: "The hobbit: An unexpected journey.",
+      rating: "3.9/5"
+    },
+    {
+      name: "Batman: The Dark Knight.",
+      rating: "4/5"
+    },
+    {
+      name:"Dolittle",
+      ratin: "3.3/5"
+    }
+  ]
+};
+
+
+
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="Navbar">
+     <h2> GoodMovies.</h2>
+       
     </div>
+     
+    <div className="backgroundimg">
+    <img src={netli} height="720" width="1279"  alt="img"/>
+    </div>
+    
+    </>
   );
 }
 
-export default App;
+export default App

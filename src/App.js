cfa3f060fbb1053ba9880e10d825f53230a9f945
Raviikumar001 <samcollins9899@gmail.com>
2021-08-25 19:57:11 +0530

    changed the design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
-import Navbar from "./components/navbar";
 import netli from '../src/images/netfli.jpg'
-
-const Movies = {
+const bookDB = {
   Marvel: [
     { name: "Doctor Strange", rating: "4/5" },
     { name: "Iron Man", rating: "3.5/5" },
@@ -45,21 +43,83 @@ const Movies = {
 
 
 
-function App()
-{
+export default function App() {
+  const [selectedGenre, setGenre] = useState("Marvel");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
   return (
-    <>
+    
+    
+    <div className="App">
     <div className="Navbar">
      <h2> GoodMovies.</h2>
        
     </div>
-     
-    <div className="backgroundimg">
-    <img src={netli} height="720" width="1279"  alt="img"/>
+      <div className="main">
+      <h1> 🎬 goodMovies </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout these Movies. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {bookDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D5DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      </div>
+      <footer class="main-footer">
+      <nav>
+          <ul class="main-footer__links">
+              <li class="main-footer__link">
+                  <a href="https://www.linkedin.com/in/ravi-kumar-5ab056143/">Linkedin</a>
+              </li>
+              <li class="main-footer__link">
+                  <a href="https://github.com/Raviikumar001">Github</a>
+              </li>
+              <li class="main-footer__link">
+                <a href="#">Medium</a>
+            </li>
+          </ul>
+      </nav>
+  </footer>
     </div>
-    
-    </>
+  
+   
   );
 }
-
-export default App
